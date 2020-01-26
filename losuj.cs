399ecdf21bcd6d2f5e4d47a5b479eaa37ef0f1e2
ImChriz <animen@go2.pl>
2020-01-26 21:56:12 +0100

    added gen random procedure

---
+++ losuj.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		public int[] tablica = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	}
+
+	static void losuj (int n)
+{
+	int[] tab = new int[n];
+	Random gen = new Random();
+
+	int i = 0;
+	while (i<n)
+    {
+		int wylosowana_liczba = gen.Next(1, 10);
+		if (!tab.Contains(wylosowana_liczba))
+		{ tab[i] = wylosowana_liczba;
+			i++;
+		}
+    }
+}
+
+}
